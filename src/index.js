3c4e8b1ddf38e8ef54bde11da01f699071442595
Khamit Amantaev <khamitamantaev@gmail.com>
2021-04-18 02:38:26 +0500

    npm install graphql

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const { ApolloServer } = require('apollo-server')
+
+
+const typeDefs = `
+    type Query {
+      info: String!
+    }
+`
+
+const resolvers = {
+  Query: {
+    info: () => `This is ApolloServer`
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+server.listen().then(({ url }) => console.log(`Server is running on ${url}`))
