44f38df53075dd01c92f7b089e1de1c8b95c3c92
Khamit Amantaev <khamitamantaev@gmail.com>
2021-04-18 03:05:44 +0500

    getusers получаю список юзеров проверяю на сопоставимость данные

--- src/index.js
+++ src/index.js
@@ -4,12 +4,32 @@ const { ApolloServer } = require('apollo-server')
 const typeDefs = `
     type Query {
       info: String!
+      users: [User!]!
+    }
+
+    type User {
+      id: ID!
+      name: String!
+      profession: String!
+      vacant: Boolean!
     }
 `
+let users = [
+  { id: 1, name: "Khamit", profession: "Developer", vacant: true},
+  { id: 2, name: "Azamat", profession: "DeveloperApp", vacant: false},
+]
 
 const resolvers = {
   Query: {
-    info: () => `This is ApolloServer`
+    info: () => `This is ApolloServer`,
+    users: () => users,
+  },
+
+  User: {
+    id: (parent) => parent.id,
+    name: (parent) => parent.name,
+    profession: (parent) => parent.profession,
+    vacant: (parent) => parent.vacant,
   }
 }
 
